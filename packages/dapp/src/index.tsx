fdc760e8ad4c76465d620d308a76c320acc1d63c
luc <luc@lucemans.nl>
2025-06-13 18:58:01 +0200

    Setup react

--- packages/dapp/src/index.tsx
+++ packages/dapp/src/index.tsx
@@ -1,15 +1,10 @@
-/* @refresh reload */
-import { render } from 'solid-js/web';
+import { App } from './App';
+import './style.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import './index.css';
-import App from './App';
-
-const root = document.getElementById('root');
-
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
-}
-
-render(() => <App />, root!);
+ReactDOM.createRoot(document.querySelector('#root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

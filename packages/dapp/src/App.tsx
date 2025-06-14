28ca567c6fa09bc6464e9683db48ba2a39d82a9a
luc <luc@lucemans.nl>
2025-06-14 15:13:14 +0200

    z

--- packages/dapp/src/App.tsx
+++ packages/dapp/src/App.tsx
@@ -1,4 +1,4 @@
-import { createWakuNode, pairExchange, sendMessage, subscribeToMessages } from 'lib';
+import mqtt from 'mqtt';
 import { QRCodeSVG } from 'qrcode.react';
 import { useState } from 'react';
 
@@ -8,10 +8,45 @@ import { useState } from 'react';
 // const encoder = createEncoder({ contentTopic, ephemeral: true });
 // const decoder = createDecoder(contentTopic);
 
-const contentTopic = '/light-guide/1/message/proto';
+// const contentTopic = '/light-guide/1/message/proto'
+const contentTopic = '/my/topic/goes/here';
 
 const initConnection = async ({ setOfferJson }: { setOfferJson: (offerJson: string) => void }) => {
-    // console.log('init conn');
+    console.log('init conn');
+
+    const client = mqtt.connect('wss://test.mosquitto.org:8081/mqtt');
+
+    console.log('client', client);
+
+    client.subscribe(contentTopic, (err, granted) => {
+        if (err) {
+            console.error('Error subscribing to topic', err);
+        }
+
+        console.log('Subscribed to topic', contentTopic, granted);
+    });
+
+    client.on('message', (topic, message) => {
+        console.log('Received message on topic', topic, message.toString());
+    });
+
+    client.on('connect', () => {
+        console.log('Connected to MQTT broker');
+    });
+
+    setInterval(() => {
+        client.publish(
+            contentTopic,
+            'hello ' + Math.round(Math.random() * 1000),
+            { qos: 1 },
+            (err) => {
+                if (err) {
+                    console.error('Error publishing message', err);
+                }
+            }
+        );
+    }, 3000);
+
     // const pc = new RTCPeerConnection({
     //     iceServers: [
     //         { urls: 'stun:stun.l.google.com:19302' },
@@ -21,14 +56,10 @@ const initConnection = async ({ setOfferJson }: { setOfferJson: (offerJson: stri
     //         // }
     //     ],
     // });
-
     // console.log('pc', pc);
-
     // // optional: open a data‐channel so connection stays alive
     // const dc = pc.createDataChannel('pair');
-
     // dc.onopen = () => console.log('data-channel open');
-
     // // 2) collect ICE + SDP
     // pc.onicecandidate = (evt) => {
     //     if (!evt.candidate) {
@@ -38,52 +69,37 @@ const initConnection = async ({ setOfferJson }: { setOfferJson: (offerJson: stri
     //             type: localDesc?.type,
     //             sdp: localDesc?.sdp,
     //         };
-
     //         console.log('payload', payload);
     //         // set here
     //         setOfferJson(JSON.stringify(payload));
     //     }
     // };
-
     // // 3) create & set local offer
     // const offer = await pc.createOffer();
-
     // console.log('offer', offer);
     // await pc.setLocalDescription(offer);
     // console.log('local desc', pc.localDescription);
     // setOfferJson(JSON.stringify(pc.localDescription));
-
-    console.log('init conn');
-
-    const node = await createWakuNode();
-
-    console.log('node', node);
-
-    const node2 = await pairExchange(node);
-
-    console.log('node2', node2);
-
-    await subscribeToMessages({
-        node,
-        contentTopic,
-        cb: (message) => {
-            console.log('message', message);
-        },
-    });
-
-    console.log('subbed');
-
-    console.log(node.libp2p.peerId);
-
-    setInterval(async () => {
-        const x = await node.getConnectedPeers();
-
-        console.log('peeeeeers', x);
-    }, 1000);
-
-    await new Promise((resolve) => setTimeout(resolve, 100));
-
-    await sendMessage({ node, contentTopic, message: 'hello' });
+    // console.log('init conn');
+    // const node = await createWakuNode();
+    // console.log('node', node);
+    // const node2 = await pairExchange(node);
+    // console.log('node2', node2);
+    // await subscribeToMessages({
+    //     node,
+    //     contentTopic,
+    //     cb: (message) => {
+    //         console.log('message', message);
+    //     },
+    // });
+    // console.log('subbed');
+    // console.log(node.libp2p.peerId);
+    // setInterval(async () => {
+    //     const x = await node.getConnectedPeers();
+    //     console.log('peeeeeers', x);
+    // }, 1000);
+    // await new Promise((resolve) => setTimeout(resolve, 100));
+    // await sendMessage({ node, contentTopic, message: 'hello' });
 };
 
 export const App = () => {
--- packages/lib/src/index.ts
+++ packages/lib/src/index.ts
@@ -1,91 +1,23 @@
-import {
-  createDecoder,
-  createEncoder,
-  createLightNode,
-  DecodedMessage,
-  LightNode,
-  Protocols,
-} from '@waku/sdk'
+export type PubSubPayload = {
 
-export const createWakuNode = () =>
-  createLightNode({
-    defaultBootstrap: true
-  })
+};
 
-export async function sendMessage(
-  { node, contentTopic, message }: {
-    node: LightNode
-    contentTopic: string
-    message: string
-  },
-) {
-  if (!node || !node.isStarted()) {
-    console.error('Waku node not started')
-    return
-  }
+export type ConnectionPayload = {
+  sessionId: string;
+  sharedKey: string;
+};
 
-  const encoder = createEncoder({ contentTopic, ephemeral: true })
-  const serialisedMessage = new TextEncoder().encode(
-    JSON.stringify({ msg: message }),
-  )
+// openlv://{sessionId}?sharedKey={sharedKey}
+// openlv://abcdefg?sharedKey=1234567890
+export const decodeConnectionURL = (url: string): ConnectionPayload => {
+  const [, sessionId, sharedKey] = url.split('?');
 
-  try {
-    await node.lightPush.send(encoder, { payload: serialisedMessage })
-    console.log('Message sent:', message)
-  } catch (error) {
-    console.error('Failed to send message:', error)
-  }
-}
+  return {
+    sessionId,
+    sharedKey,
+  };
+};
 
-// Subscribe to incoming messages
-export async function subscribeToMessages(
-  { node, contentTopic, cb }: {
-    node: LightNode
-    contentTopic: string
-    cb: (message: DecodedMessage) => void
-  },
-) {
-  if (!node || !node.isStarted()) {
-    console.error('Waku node not started')
-    return
-  }
-
-  const decoder = createDecoder(contentTopic)
-  const callback = (wakuMessage: DecodedMessage) => {
-    if (!wakuMessage.payload) return
-    const messageObj = JSON.parse(
-      new TextDecoder().decode(wakuMessage.payload),
-    )
-    console.log('Received message:', messageObj)
-    // Send received message to frontend
-    cb(wakuMessage)
-  }
-
-  try {
-    const { error, subscription } = await node.filter.subscribe(
-      decoder,
-      callback,
-    )
-    if (error || !subscription) {
-      console.error('Subscription error:', error)
-    } else {
-      console.log('Subscribed to messages')
-    }
-  } catch (error) {
-    console.error('Subscription failed:', error)
-  }
-}
-
-export const pairExchange = async (node: LightNode) => {
-  console.log('starting node');
-
-  await node.start()
-
-  console.log('started node. waiting for peers');
-
-  await node.waitForPeers()
-
-  console.log('peers', node.peerId);
-
-  return node
-}
+export const encodeConnectionURL = (payload: ConnectionPayload) => {
+  return `openlv://${payload.sessionId}?sharedKey=${payload.sharedKey}`;
+};

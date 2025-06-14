6497e0246a3218e4c2760402d4f7de5da3c2aa70
v1rtl <hi@v1rtl.site>
2025-06-14 15:30:05 +0200

    wip

--- packages/wallet/src/utils/tryConnect.ts
+++ packages/wallet/src/utils/tryConnect.ts
@@ -1,11 +1,21 @@
-import { contentTopic, decodeConnectionURL } from "lib"
+import { contentTopic, decodeConnectionURL, startConnection } from "lib"
 
 export const tryConnect = async (result: unknown) => {
     try {
         const {sessionId, sharedKey } = decodeConnectionURL(result as string)
        
-        const topic = contentTopic(sessionId)
+        const topic = contentTopic({sessionId})
 
+
+        const client = startConnection()
+
+        client.subscribe(topic, (err, granted) => {
+            if (err) {
+                console.error('Error subscribing to topic', err);
+            }
+
+            console.log('Subscribed to topic', topic, granted);
+        });
     } catch (error) {
         throw new Error(`Failed to pair with host: ${error}`, {cause: error})
     }

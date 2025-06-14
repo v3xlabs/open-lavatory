cfe81fc4fc279ee6bd8b4b1ac37c7c65a7d82a8e
v1rtl <hi@v1rtl.site>
2025-06-14 14:09:19 +0200

    connect to bootnode on mount

--- packages/wallet/src/App.tsx
+++ packages/wallet/src/App.tsx
@@ -1,12 +1,18 @@
 import type { Component } from 'solid-js';
-import { createSignal, Show } from 'solid-js'
-
+import { createSignal, onMount, Show } from 'solid-js'
+import { createWakuNode, pairExchange } from 'lib'
 import styles from './App.module.css';
 import { QRScanner } from './components/QRScanner';
 
 const App: Component = () => {
 
   const [show, setShow] = createSignal(false)
+
+  onMount(async () => {
+    const node = await createWakuNode()
+
+    await pairExchange(node)
+  })
 
   return (
     <main class={styles.main}>

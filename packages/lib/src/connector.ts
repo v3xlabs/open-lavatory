07a9be97896cd14f016805fff0fadc39d946460d
v1rtl <hi@v1rtl.site>
2025-06-14 17:21:15 +0200

    stub connector

---
+++ packages/lib/src/connector.ts
@@ -0,0 +1,32 @@
+import { createConnector } from 'wagmi'
+
+export const openLvConnector = createConnector((config) => ({
+    connect: async (parameters) => {
+        return { accounts:[], chainId:1 }
+    },
+    disconnect: async () => { 
+        
+    },
+    id: 'openLv',
+    name: 'OpenLV',
+    getAccounts: async () => {
+        return []
+    },
+    getChainId: async () => {
+        return 1
+    },
+ getProvider(parameters) {
+     
+ },
+   isAuthorized: async () => {
+       return true
+   },
+   type: 'wallet',
+   onAccountsChanged: async () => {
+       
+   },
+   onChainChanged: async () => {
+       
+   },
+   onDisconnect: async () => { },
+}))

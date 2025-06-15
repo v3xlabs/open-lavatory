bcee9398171d35528b06568798c4e36adb1b058b
v1rtl <hi@v1rtl.site>
2025-06-15 11:45:18 +0200

    ui

--- packages/wallet/src/components/WalletInfo.tsx
+++ packages/wallet/src/components/WalletInfo.tsx
@@ -1,4 +1,4 @@
-import { useBalance } from 'wagmi'
+import { useBalance, useClient } from 'wagmi'
 import { ANVIL_ACCOUNT } from '../../lib/anvil'
 import { formatEther } from 'viem'
 import styles from './WalletInfo.module.css'
@@ -17,10 +17,22 @@ const Balance = () => {
   return <div className={styles.balance}>{formatted.slice(0, formatted.indexOf('.'))} <img height={32} width={32} src="/eth.svg" alt="ETH" /></div>
 }
 
+const Address = () => {
+  return <div className={styles.address}>{ANVIL_ACCOUNT.slice(0, 6)}...{ANVIL_ACCOUNT.slice(-4)}</div>
+}
+
+const Chain = () => {
+    const client = useClient()
+
+  return <div>chain: {client?.chain.name}</div>
+}
+
 export const WalletInfo = () => {
   return (
     <div className={styles.container}>
       <Balance />
+      <Address />
+      <Chain />
     </div>
   )
 }

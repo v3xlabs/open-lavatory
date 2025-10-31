4ffc867b0afeab05aa569b0c5d4f4b221c80effe
waiscodes <58951863+waiscodes@users.noreply.github.com>
2025-10-31 13:34:35 -0500

    Add modal settings ui

--- packages/modal/src/icons/index.tsx
+++ packages/modal/src/icons/index.tsx
@@ -44,3 +44,23 @@ export const CogIcon = ({ size = 24 }: { size?: number }) => (
     />
   </svg>
 );
+
+export const ChevronDownIcon = ({ size = 24 }: { size?: number }) => (
+    // biome-ignore lint/a11y/noSvgWithoutTitle: Chevron down icon
+    <svg
+        title="Chevron down icon"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <path
+            d="M6 9L12 15L18 9"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        />
+    </svg>
+);

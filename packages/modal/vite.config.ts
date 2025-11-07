069bbec15b9c090b317436ca290ef26826caab4a
Luc <luc@lucemans.nl>
2025-11-07 16:18:25 +0100

    Update modal vite build

--- packages/modal/vite.config.ts
+++ packages/modal/vite.config.ts
@@ -1,15 +1,21 @@
 import preact from "@preact/preset-vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [
+    preact(),
+    tailwindcss(),
+    dts({ insertTypesEntry: true, outDir: "dist", rollupTypes: true }),
+  ],
   build: {
     lib: {
+      name: "OpenLVModal",
       entry: "src/index.ts",
-      formats: ["es"],
-      fileName: () => "index.js",
+      formats: ["es", "cjs", "iife"],
+      fileName: (format) => `index.${format}.js`,
     },
     sourcemap: true,
     target: "es2020",

9b52487a46e150b54ff8f1ab9311627e3ff73a81
Luc <luc@lucemans.nl>
2025-11-07 03:02:59 +0100

    Cleanup react components

--- packages/modal/src/components/ui/menu/MenuGroup.tsx
+++ packages/modal/src/components/ui/menu/MenuGroup.tsx
@@ -5,18 +5,14 @@ export type MenuGroupProps = PropsWithChildren<{
   right?: ReactNode;
 }>;
 
-export const MenuGroup: FC<MenuGroupProps> = ({ title, right, children }) => {
-  //
-
-  return (
-    <div>
-      <div className="flex items-end justify-between p-2">
-        <div>{title}</div>
-        <div>{right}</div>
-      </div>
-      <div className="flex flex-col gap-1 rounded-md bg-[#F4F5F6] p-2">
-        {children}
-      </div>
+export const MenuGroup: FC<MenuGroupProps> = ({ title, right, children }) => (
+  <div>
+    <div className="flex items-end justify-between p-2">
+      <div>{title}</div>
+      <div>{right}</div>
     </div>
-  );
-};
+    <div className="flex flex-col gap-1 rounded-md bg-[#F4F5F6] p-2">
+      {children}
+    </div>
+  </div>
+);
--- packages/modal/src/components/ui/menu/MenuItem.tsx
+++ packages/modal/src/components/ui/menu/MenuItem.tsx
@@ -4,11 +4,9 @@ export type MenuItemProps = PropsWithChildren<{
   label: string;
 }>;
 
-export const MenuItem: FC<MenuItemProps> = ({ label, children }) => {
-  return (
-    <div className="flex items-baseline justify-between gap-2">
-      <div className="pl-1 text-sm">{label}</div>
-      <div>{children}</div>
-    </div>
-  );
-};
+export const MenuItem: FC<MenuItemProps> = ({ label, children }) => (
+  <div className="flex items-baseline justify-between gap-2">
+    <div className="pl-1 text-sm">{label}</div>
+    <div>{children}</div>
+  </div>
+);

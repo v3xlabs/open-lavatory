3ea7ec36045a7e248c5cc4a77256f680108b7ce9
Luc van Kampen <luc@lucemans.nl>
2026-02-03 16:13:24 -0300

    Update menu padding (#70)

--- packages/modal/src/ui/menu/MenuItem.tsx
+++ packages/modal/src/ui/menu/MenuItem.tsx
@@ -5,7 +5,7 @@ export type MenuItemProps = PropsWithChildren<{
 }>;
 
 export const MenuItem: FC<MenuItemProps> = ({ label, children }) => (
-  <div className="flex items-baseline justify-between gap-2 px-2 py-1">
+  <div className="flex items-baseline justify-between gap-2 px-2 py-1 first:pt-2 last:pb-2">
     <div className="text-sm text-(--lv-text-secondary) text-start">{label}</div>
     <div>{children}</div>
   </div>

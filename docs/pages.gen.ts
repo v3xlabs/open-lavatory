7227f6e6ec87f11fcf0d7312e0047404530a8ec6
Jonatan Holmgren <jonatan@jontes.page>
2026-06-02 21:41:25 +0200

    Revert pages.gen.ts

--- docs/pages.gen.ts
+++ docs/pages.gen.ts
@@ -1,38 +1,38 @@
 // deno-fmt-ignore-file
 // biome-ignore format: generated types do not need formatting
 // prettier-ignore
-import type { PathsForPages } from "waku/router";
+import type { PathsForPages } from 'waku/router'
 
 // prettier-ignore
 type Page =
-  | { path: "/api/connector"; render: "static"; }
-  | { path: "/api/intro"; render: "static"; }
-  | { path: "/api/provider"; render: "static"; }
-  | { path: "/api/session"; render: "static"; }
-  | { path: "/api/signaling/gun"; render: "static"; }
-  | { path: "/api/signaling"; render: "static"; }
-  | { path: "/api/signaling/mqtt"; render: "static"; }
-  | { path: "/api/signaling/ntfy"; render: "static"; }
-  | { path: "/api/transport"; render: "static"; }
-  | { path: "/api/transport/webrtc"; render: "static"; }
-  | { path: "/api/uri"; render: "static"; }
-  | { path: "/faq"; render: "static"; }
-  | { path: "/getting-started"; render: "static"; }
-  | { path: "/guides/configuration"; render: "static"; }
-  | { path: "/guides/theme"; render: "static"; }
-  | { path: "/how"; render: "static"; }
-  | { path: "/"; render: "static"; }
-  | { path: "/try"; render: "static"; }
-  | { path: "/wallets"; render: "static"; }
-  | { path: "/wallets/migrate"; render: "static"; }
-  | { path: "/wallets/react-native"; render: "static"; };
+  | { path: '/api/connector'; render: 'static' }
+  | { path: '/api/intro'; render: 'static' }
+  | { path: '/api/provider'; render: 'static' }
+  | { path: '/api/session'; render: 'static' }
+  | { path: '/api/signaling/gun'; render: 'static' }
+  | { path: '/api/signaling'; render: 'static' }
+  | { path: '/api/signaling/mqtt'; render: 'static' }
+  | { path: '/api/signaling/ntfy'; render: 'static' }
+  | { path: '/api/transport'; render: 'static' }
+  | { path: '/api/transport/webrtc'; render: 'static' }
+  | { path: '/api/uri'; render: 'static' }
+  | { path: '/faq'; render: 'static' }
+  | { path: '/getting-started'; render: 'static' }
+  | { path: '/guides/configuration'; render: 'static' }
+  | { path: '/guides/theme'; render: 'static' }
+  | { path: '/how'; render: 'static' }
+  | { path: '/'; render: 'static' }
+  | { path: '/try'; render: 'static' }
+  | { path: '/wallets'; render: 'static' }
+  | { path: '/wallets/migrate'; render: 'static' }
+  | { path: '/wallets/react-native'; render: 'static' }
 
 // prettier-ignore
-declare module "waku/router" {
+declare module 'waku/router' {
   interface RouteConfig {
-    paths: PathsForPages<Page>;
+    paths: PathsForPages<Page>
   }
   interface CreatePagesConfig {
-    pages: Page;
+    pages: Page
   }
 }
